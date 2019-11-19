8b3ef8e7a4df3709a5034d4dd50720e09b56559f
Uros Nikolic <nikolicu@yahoo.com>
2019-11-19 16:36:35 +0100

    Fact sheets sub category sort fix

--- src/app/components/pages/fact-sheet-archive/fact-sheet-archive.component.ts
+++ src/app/components/pages/fact-sheet-archive/fact-sheet-archive.component.ts
@@ -57,7 +57,7 @@ export class FactSheetArchiveComponent implements OnInit {
     this.categoryData = null;
     this.wpservice.getCategory(`?parent=${category.id}&per_page=20`).subscribe(subCate => {
       this.subCategory = subCate;
-      this.subCategory.sort((a, b) => b.name - a.name);
+      this.subCategory.sort((b, a) => b.name - a.name);
       this.subCategory = subCate;
       console.log(this.subCategory);
     });
